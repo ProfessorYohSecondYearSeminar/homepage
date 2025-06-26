9fde9456a008cba49576859250bee50282b498c5
keitaka8181 <keitaka8181@gmail.com>
2025-06-26 11:06:24 +0900

    add image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div
@@ -5,6 +7,12 @@ export default function Home() {
       style={{ backgroundColor: "#24377C" }}
     >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <Image
+        src="/160531091017_1.jpg"
+        alt="TAKASHIMAMAINIMAGE"
+        width={300}
+        height={200}
+      />
         <h1 className="text-white text-2xl font-bold">Welcome to Our Yoh Second Year Seminars Site</h1>
       </main>
     </div>
